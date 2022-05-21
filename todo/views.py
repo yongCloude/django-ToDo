3f4f2e220edaa4bdf9237b29b320fe5ec97cac99
yongCloude <djkye0001@naver.com>
2022-05-22 00:05:30 +0900

    [fix] import 실수 수정

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,13 @@
 from rest_framework import status
-from rest_framework import Response
-from rest_framework import APIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import viewsets
 
 from .models import Todo
 from .serializers import TodoSimpleSerializer
 
 # Create your views here.
-class TodoAPIView(APIView) :
+class TodosAPIView(APIView) :
     """
         Todo total search View
     """        
@@ -15,4 +15,4 @@ class TodoAPIView(APIView) :
         todos = Todo.objects.filter(complete = False)
         serializer = TodoSimpleSerializer(todos, many = True)
         return Response(serializer.data, status = status.HTTP_200_OK)
-        
+
