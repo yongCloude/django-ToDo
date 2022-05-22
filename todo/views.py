9d3005f78ab565b9dfbafe4cf59ed77d7dcc0a3b
yongCloude <djkye0001@naver.com>
2022-05-22 16:33:25 +0900

    [feat] Todo List 생성 View

--- todo/views.py
+++ todo/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework import viewsets
 
 from .models import Todo
-from .serializers import TodoSimpleSerializer
+from .serializers import TodoCreateSerializer, TodoSimpleSerializer
 
 # Create your views here.
 class TodosAPIView(APIView) :
@@ -16,6 +16,14 @@ class TodosAPIView(APIView) :
         todos = Todo.objects.filter(complete = False)
         serializer = TodoSimpleSerializer(todos, many = True)
         return Response(serializer.data, status = status.HTTP_200_OK)
+    
+    def post(self, request) :
+        serializer = TodoCreateSerializer(data = request.data)        
+        if serializer.is_valid() :
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
 
 class TodoAPIView(APIView) :
     """
